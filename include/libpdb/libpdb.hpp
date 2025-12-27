57ad905298612e5b74a37713120217cec89c500d
pbmoeller <sonla@gmx.net>
2025-12-27 19:53:07 +0100

    Initial setup. Added Tools, Tests, Library and vcpkg library dependencies.

---
+++ include/libpdb/libpdb.hpp
@@ -0,0 +1,10 @@
+#ifndef PDB_LIBPDB_HPP
+#define PDB_LIBPDB_HPP
+
+namespace pdb {
+
+void say_hello();
+
+} // namespace pdb
+
+#endif // PDB_LIBPDB_HPP
---
+++ tools/pdb.cpp
@@ -0,0 +1,7 @@
+#include <libpdb/libpdb.hpp>
+
+int main(int argc, char** argv)
+{
+    pdb::say_hello();
+    return 0;
+}
