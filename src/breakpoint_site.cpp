9ee88dc6559d134fff62cbf4709532d04a22abdf
pbmoeller <sonla@gmx.net>
2026-01-01 22:39:59 +0100

    Implemented enable and disable breakpoints

--- src/breakpoint_site.cpp
+++ src/breakpoint_site.cpp
@@ -1,4 +1,8 @@
 #include <libpdb/breakpoint_site.hpp>
+#include <libpdb/error.hpp>
+#include <libpdb/process.hpp>
+
+#include <sys/ptrace.h>
 
 namespace pdb {
 
@@ -19,6 +23,48 @@ BreakpointSite::BreakpointSite(Process& process, VirtAddr address)
     , m_savedData{}
 {
     m_id = getNextId();
+}
+
+void BreakpointSite::enable()
+{
+    if(m_isEnabled) {
+        return;
+    }
+
+    errno         = 0;
+    uint64_t data = ptrace(PTRACE_PEEKDATA, m_process->pid(), m_address, nullptr);
+    if(errno != 0) {
+        Error::sendErrno("Enabling BreakpointSite failed");
+    }
+
+    m_savedData           = static_cast<std::byte>(data & 0xFF);
+    uint64_t int3         = 0xCC;
+    uint64_t dataWithInt3 = ((data & ~0xFF) | int3);
+
+    if(ptrace(PTRACE_POKEDATA, m_process->pid(), m_address, dataWithInt3) < 0) {
+        Error::sendErrno("Enabling BreakpointSite failed");
+    }
+    m_isEnabled = true;
+}
+
+void BreakpointSite::disable()
+{
+    if(!m_isEnabled) {
+        return;
+    }
+
+    errno         = 0;
+    uint64_t data = ptrace(PTRACE_PEEKDATA, m_process->pid(), m_address, nullptr);
+    if(errno != 0) {
+        Error::sendErrno("Enabling BreakpointSite failed");
+    }
+
+    auto restoredData = ((data & ~0xFF) | static_cast<uint8_t>(m_savedData));
+    if(ptrace(PTRACE_POKEDATA, m_process->pid(), m_address, restoredData) < 0) {
+        Error::sendErrno("Enabling BreakpointSite failed");
+    }
+
+    m_isEnabled = false;
 }
 
 } // namespace pdb
